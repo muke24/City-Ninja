b7a5934a5911f721e5c08a35633ecf4edcfa4761
Peter <pjthompson.au@gmail.com>
2020-02-04 16:41:04 +1100

    MainMenu

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public string SceneName;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}

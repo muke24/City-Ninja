5477eff0af0612153059b6fa6e1cb12b21abf697
Peter <pjthompson.au@gmail.com>
2020-02-04 20:02:16 +1100

    Lots of stuff

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public string sceneName;
+    public GameObject mainMenu;
+    public GameObject settings;
+    public bool settingsOpen = false;
+    public bool menuOpen = true;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void OpenSettings()
+    {
+        
+        if (mainMenu.activeInHierarchy == true)
+        {
+            settingsOpen = true;
+            menuOpen = false;
+            mainMenu.SetActive(false);
+            settings.SetActive(true);            
+        }
+
+    }
+    
+    public void OpenMenu()
+    {
+        if (settings.activeInHierarchy == true)
+        {
+            settingsOpen = false;
+            menuOpen = true;
+            mainMenu.SetActive(true);
+            settings.SetActive(false);
+        }
+
+    }
+
+}

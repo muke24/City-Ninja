1b8732df4c08d886bacdface0166e4a72084e363
Peter <pjthompson.au@gmail.com>
2020-02-24 15:44:31 +1100

    stuff

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -5,56 +5,67 @@ using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
-    public Slider volumeSlider;
-    public Slider resolutionSlider;
+	public Slider volumeSlider;
+	public Slider resolutionSlider;
 
-    public Text volumeText;
-    public Text resolutionText;
-    public Text resolutionSize;
+	public Text volumeText;
+	public Text resolutionText;
+	public Text resolutionSize;
 
-    public string resText;
+	public string resText;
 
-    public int volumeInt;
-    public int resolutionIntWidth;
-    public int resolutionIntHeight;
+	public int volumeInt;
+	public int resolutionIntWidth;
+	public int resolutionIntHeight;
 
-    public int resWidth;
-    public int resHeight;
+	public int resWidth;
+	public int resHeight;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        resWidth = Screen.currentResolution.width;
-        resHeight = Screen.currentResolution.height;
-    }
+	public bool resChanged;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+	// Start is called before the first frame update
+	void Start()
+	{
+		resChanged = false;
+		resWidth = Screen.width;
+		resHeight = Screen.height;
+	}
 
-        resolutionIntWidth = Mathf.RoundToInt(resWidth * resolutionSlider.value);
-        resolutionIntHeight = Mathf.RoundToInt(resHeight * resolutionSlider.value);
+	// Update is called once per frame
+	void Update()
+	{
 
-        resolutionText.text = resText;
-        resText = resolutionSlider.value.ToString();
-        if (resText.Length > 3)
-        {
-            resText = resText.Substring(0, 3);
-        }
 
-        volumeInt = Mathf.RoundToInt(volumeSlider.value);
-        volumeText.text = volumeSlider.value.ToString() + "%";
-        
+		resolutionIntWidth = Mathf.RoundToInt(resWidth * resolutionSlider.value);
+		resolutionIntHeight = Mathf.RoundToInt(resHeight * resolutionSlider.value);
 
-        resolutionSize.text = resolutionIntWidth.ToString() + " x " + resolutionIntHeight.ToString();
-    }
+		resolutionText.text = resText;
+		resText = resolutionSlider.value.ToString();
+		if (resText.Length > 3)
+		{
+			resText = resText.Substring(0, 3);
+		}
 
-    public void ApplySettings()
-    {
-        Screen.SetResolution(resolutionIntWidth, resolutionIntHeight, FullScreenMode.ExclusiveFullScreen);
-    }
+		volumeInt = Mathf.RoundToInt(volumeSlider.value);
+		volumeText.text = volumeSlider.value.ToString() + "%";
 
+
+		resolutionSize.text = resolutionIntWidth.ToString() + " x " + resolutionIntHeight.ToString();
+	}
+
+	public void ApplySettings()
+	{
+		if (resChanged == true)
+		{
+			Screen.SetResolution(resolutionIntWidth, resolutionIntHeight, FullScreenMode.ExclusiveFullScreen);
+			resChanged = false;
+		}
+		AudioListener.volume = volumeSlider.value;
+	}
+	public void ResolutionChanged()
+	{
+		resChanged = true;
+	}
 
 
 }

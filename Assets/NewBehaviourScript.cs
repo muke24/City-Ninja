f1a783909fb312de5fff62ac82e5170aa8602f52
Peter <pjthompson.au@gmail.com>
2020-02-25 13:02:45 +1100

    ParticleEffectWorking

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject PoolPrefab;
+    List<GameObject> PooledGameObjects;
+    // List<GameObject> SpawnedGameObjects;
+
+    public GameObject Spawn()
+    {
+
+        GameObject returnGO;
+
+        if (PooledGameObjects.Count == 0)
+        {
+            returnGO = Instantiate(PoolPrefab);
+        }
+        else
+        {
+            returnGO = PooledGameObjects[0];
+            PooledGameObjects.RemoveAt(0);
+        }
+
+        return returnGO;
+    }
+
+    public void DeSpawn(GameObject despawnObject)
+    {
+        PooledGameObjects.Add(despawnObject);
+
+        despawnObject.SetActive(false);
+
+        despawnObject.transform.SetParent(null);
+    }
+}
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public ParticleSystem particle;
-    
-    public int damage = 1;
-    public float speed;
+	public ParticleSystem particle;
+	public GameObject obstacle;
 
-    private void Update()
-    {
-        transform.Translate(Vector2.left * speed * Time.deltaTime); //obstacles move direction
-    }
+	public int damage = 1;
+	public float speed;
 
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.CompareTag("Player")) //if the obstacle collides with a "Player" tag then deal damage and destroy self
-        {
-            Instantiate(particle);
-            
-            collision.GetComponent<CharacterMovement>().health -= damage;
-            Debug.Log(collision.GetComponent<CharacterMovement>().health);
-            Destroy(gameObject);
-        }
-    }
+
+	private void Update()
+	{
+		transform.Translate(Vector2.left * speed * Time.deltaTime); //obstacles move direction
+
+
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player")) //if the obstacle collides with a "Player" tag then deal damage and destroy self
+		{
+
+			Instantiate(particle, obstacle.transform.position, obstacle.transform.rotation);
+
+
+			collision.GetComponent<CharacterMovement>().health -= damage;
+			Debug.Log(collision.GetComponent<CharacterMovement>().health);
+
+			Destroy(gameObject);
+		}
+	}
+
+
 }

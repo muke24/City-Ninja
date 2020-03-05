2027b9740446af1d1511c7d3767a2a73086ab7a4
ramkillnani <54158723+ramkillnani@users.noreply.github.com>
2020-03-05 14:53:04 +1100

    COMPLETE BITCHES

---
+++ Assets/Scripts/OffscreenDeath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffscreenDeath : MonoBehaviour
+{
+    public int damage = 3;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.GetComponent<CharacterMovement>().health -= damage;
+        }
+    }
+
+
+}
